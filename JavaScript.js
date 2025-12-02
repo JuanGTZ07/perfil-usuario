3bd08f24147d258e4fc5a5efddabdf498a954a83
JuanGTZ07 <al45677@un.edu.mx>
2025-12-02 10:09:09 -0600

    JavaScript casi terminado

--- JavaScript.js
+++ JavaScript.js
@@ -1,5 +1,16 @@
-
+let nombre = document.getElementById("nombre");
+let ocupacion = document.getElementById("ocupacion");
+let card = document.querySelector(".perfil-card");
+card.addEventListener("click", cambio);
 
 function cambio() {
-    
+    if (nombre.textContent.trim() === "Mariano") {
+        nombre.textContent = "Mariana";
+    }
+
+    if (ocupacion.textContent.trim() === "Doctor") {
+        ocupacion.textContent = "Doctora";
+    }
+
+    alert("Cambios realizados");
 }
